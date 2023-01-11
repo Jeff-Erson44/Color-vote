5fa1a702d613504419ffdbf3f78bb430198751ef
Jeff-Erson44 <jeffersonk04.pro@gmail.com>
2023-01-11 17:01:10 +0100

    [ADD] Navbar

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,7 +1,21 @@
+import Navbar from '../../components/Navbar'
+import styled from'styled-components'
+
+const DashboardStyled = styled.section`
+    width: 80vw;
+    margin-left: 20vw;
+`
 export default function Dashboard() {
     return(
         <>
-            
+            <Navbar />
+
+            <DashboardStyled>
+                <h1>Tableau de bord</h1>
+                <h2>Résultats du dernier questionnaire</h2>
+
+                <h3>Vous êtes connecté en tant qu'Admin</h3>
+            </DashboardStyled>
         </>
     )
 }
