02b7b3bc59aae499af05dbf3b505a610de06fd88
Jeff-Erson44 <jeffersonk04.pro@gmail.com>
2023-01-11 14:52:50 +0100

    [ADD] Font Helvetica

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,9 @@ export default function Document() {
     <Html lang="fr">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="preload" href="/fonts/HelveticaNeueCyr-Medium.woff" as="font" type='font/woff'crossOrigin="anonymous"/>
+        <link rel="preload" href="/fonts/HelveticaNeueCyr-Light.woff" as="font" type='font/woff'crossOrigin="anonymous"/>
+        <link rel="preload" href="/fonts/HelveticaNeueCyr-Bold.woff" as="font" type='font/woff'crossOrigin="anonymous"/>
       </Head>
       <body>
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 
+
 export default function Home() {
   
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <h1>Home Page</h1>
     </>
   )
