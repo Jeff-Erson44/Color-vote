8365a5d248ae7e40d20d7ebcab856b7d4e2c9a69
Aaouis <z_aaouis@stu-digital-campus.fr>
2023-01-13 10:48:08 +0100

    [ADD] Matrice

---
+++ pages/dashboard/Matrice.js
@@ -0,0 +1,529 @@
+import styled from "styled-components"
+import Image from 'next/image'
+import Navbar from '../../components/Navbar'
+
+
+const MatriceStyled = styled.div`
+
+.page-header h1{
+position: absolute;
+width: 163px;
+height: 39px;
+left: 420px;
+top: 165px;
+font-family: 'Helvetica Neue';
+font-style: normal;
+font-weight: 700;
+font-size: 32px;
+line-height: 39px;
+color: #000000;
+}
+
+.page-header h2{
+position: absolute;
+width: 401px;
+height: 20px;
+left: 420px;
+top: 250px;
+font-family: 'Helvetica Neue';
+font-style: normal;
+font-weight: 40;
+font-size: 18px;
+line-height: 30px;
+color: #000000;
+    }
+
+.page-header h10{
+position: absolute;
+width: 401px;
+height: 20px;
+left: 970px;
+top: 265px;
+font-family: 'Helvetica Neue';
+font-style: normal;
+font-weight: 40;
+font-size: 18px;
+line-height: 30px;
+color: #000000;
+    }
+
+    .page-header h11{
+        position: absolute;
+        width: 401px;
+        height: 20px;
+        left: 1574px;
+        top: 265px;
+        font-family: 'Helvetica Neue';
+        font-style: normal;
+        font-weight: 40;
+        font-size: 18px;
+        line-height: 30px;
+        color: #000000;
+            }
+
+
+.page-header h3{
+position: absolute;  
+left: 420px;
+top: 350px;
+width: 75px;
+height: 42px;
+font-family: 'Helvetica Neue';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 21px;
+color: #000000;
+ 
+}
+
+.page-header h4{
+    position: absolute;  
+    left: 400px;
+    top: 345px;
+    width: 75px;
+    height: 42px;
+    font-family: 'Helvetica Neue';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 21px;
+    color: #000000;
+     
+    }
+
+    .page-header h5{
+        position: absolute;  
+        left: 180px;
+        top: 340px;
+        width: 75px;
+        height: 42px;
+        font-family: 'Helvetica Neue';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 21px;
+        color: #000000;
+         
+        }
+
+
+        .page-header h6{
+            position: absolute;  
+            left: 550px;
+            top: 325px;
+            width: 75px;
+            height: 42px;
+            font-family: 'Helvetica Neue';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 18px;
+            line-height: 21px;
+            color: #000000;
+             
+            }
+
+
+.page-header h7{
+             position: absolute;  
+                left: 680px;
+                top: 370px;
+                width: 75px;
+                height: 42px;
+                font-family: 'Helvetica Neue';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 18px;
+                line-height: 21px;
+                color: #000000;
+                 
+                }
+
+.page-header h8{
+position: absolute;  
+ left: 850px;
+ top: 370px;
+width: 75px;
+height: 42px;
+ font-family: 'Helvetica Neue';
+ font-style: normal;
+font-weight: 400;
+font-size: 18px;
+ line-height: 21px;
+ color: #000000;
+                     
+                    }
+
+.page-header h9{
+position: absolute;  
+ left: 1000px;
+ top: 370px;
+width: 75px;
+height: 42px;
+font-family: 'Helvetica Neue';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+ line-height: 21px;
+color: #000000;
+                                             
+}
+
+
+.page-header h12{
+    position: absolute;  
+     left: 1130px;
+     top: 370px;
+    width: 75px;
+    height: 42px;
+    font-family: 'Helvetica Neue';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+     line-height: 21px;
+    color: #000000;
+                                                 
+    }
+    .page-header h13{
+        position: absolute;  
+         left: 1280px;
+         top: 370px;
+        width: 75px;
+        height: 42px;
+        font-family: 'Helvetica Neue';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 18px;
+         line-height: 21px;
+        color: #000000;
+                                                     
+        }
+
+
+        .page-header h14{
+            position: absolute;  
+             left: 1440px;
+             top: 370px;
+            width: 75px;
+            height: 42px;
+            font-family: 'Helvetica Neue';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 18px;
+             line-height: 21px;
+            color: #000000;
+                                                         
+            }
+
+
+
+            .page-header h15{
+                position: absolute;  
+                 left: 1640px;
+                 top: 370px;
+                width: 75px;
+                height: 42px;
+                font-family: 'Helvetica Neue';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 18px;
+                 line-height: 21px;
+                color: #000000;
+                                                             
+                }
+
+   .container .content{
+    position: absolute;
+    width: 88px;
+    height: 51px;
+    left: 415px;
+    top: 450px;
+    background: #FF8C00;
+   }
+
+
+.container1 .content1{
+position: absolute;
+width: 123px;
+height: 51px;
+left: 500px;
+top: 450px;
+background: #008000;
+   }  
+   
+   
+.container2 .content2{
+position: absolute;
+width: 106px;
+height: 51px;
+left: 600px;
+top: 450px;
+background: rgba(0, 0, 0, 0.99);
+       }
+
+.container3 .content3{
+    position: absolute;
+    width: 500px;
+    height: 51px;
+    left: 700px;
+    top: 450px;
+    
+    
+    background: #008000;
+}
+
+.container4 .content4{
+position: absolute;
+width: 200px;
+height: 51px;
+left: 901px;
+top: 450px;
+
+background: #990A00;
+}
+
+.container5 .content5{
+    position: absolute;
+    width: 700px;
+    height: 51px;
+    left: 1020px;
+    top: 450px;
+        
+    background: #008000;   
+}
+
+.container6 .content6{
+position: absolute;
+width: 250px;
+height: 51px;
+left: 1500px;
+top: 450px;
+background: #E82300
+}
+
+
+
+
+.container7 .content7{
+    position: absolute;
+    width: 88px;
+    height: 51px;
+    left: 415px;
+    top: 550px;
+    background: #FF8C00;
+       } 
+       
+       .container8 .content8{
+        position: absolute;
+        width: 123px;
+        height: 51px;
+        left: 500px;
+        top: 550px;
+        background: #008000;
+           } 
+           
+.container9 .content9{
+position: absolute;
+width: 106px;
+height: 51px;
+left: 600px;
+top: 550px;
+background: rgba(0, 0, 0, 0.99);
+       }        
+
+    .container10 .content10{
+        position: absolute;
+    width: 500px;
+    height: 51px;
+    left: 700px;
+    top: 550px;
+    background: #008000;
+    }
+
+    .container11 .content11{
+        position: absolute;
+        width: 120px;
+        height: 51px;
+        left: 901px;
+        top: 550px;
+        background: #990A00;
+    }
+    
+    .container12 .content12{
+    position: absolute;
+    width: 680px;
+    height: 51px;
+    left: 1020px;
+    top: 550px;
+        
+    background: #008000;
+
+    }
+
+
+    .container13 .content13{
+        position: absolute;
+        width: 250px;
+        height: 51px;
+        left: 1500px;
+        top: 550px;
+        background: #E82300
+    }
+
+    
+
+`
+
+
+
+
+export default function Matrice() {
+    return(
+        <>
+
+
+           <MatriceStyled>
+           <Navbar />
+
+                <ul className="container__navbar--logo">
+                    <li>
+                        <Image
+                            src="/img/ColorVote.png"
+                            alt="logo"
+                            width={130}
+                            height={100}
+                        />
+                    </li>
+                </ul>
+
+                <main>
+                    <div class="page-header">
+                    <div>
+
+                        <h1> La matrice</h1>
+                        <h2> Session : Sport</h2>
+                        <h10>Nombre de participants : 18</h10>
+                        <h11>Nombre de question : 40</h11>
+                        <h3> Jefferson Kouao</h3>
+                        <h6> Younes Kadiri </h6>
+                        <h7> Abidesh Thayanandan</h7>
+                        <h8>Renaud Vernageau</h8>
+                        <h9>Alexis Novinc</h9>
+                        <h12>Zakariae Aaouis</h12>
+                        <h13>Quentin Lapujade</h13>
+                        <h14>Elisabeth Ngo</h14>
+                        <h15>Yarline Cineus</h15>
+
+                        
+
+                    </div>  
+                    </div>
+                </main>
+
+                <div class="container">
+                <div class="content">  
+                    
+                    </div>
+
+                    </div>
+
+                    <div class="container1">  
+                <div class="content1">  
+                    
+                    </div>
+
+                    </div>
+
+                    <div class="container2">  
+                <div class="content2">  
+                    
+                    </div>
+
+                    </div>
+
+                    <div class="container3">  
+                <div class="content3">  
+                    
+                    </div>
+
+                    </div>
+                
+                    <div class="container4">  
+                <div class="content4">  
+                    
+                    </div>
+
+                    </div>
+
+                    <div class="container5">  
+                <div class="content5">  
+                    
+                    </div>
+
+                    </div>
+
+
+                    <div class="container6">  
+                <div class="content6">  
+                    
+                    </div>
+
+                    </div>
+
+
+
+
+                    <div class="container7">  
+                <div class="content7">  
+                    
+                    </div>
+
+                    </div>
+
+                    <div class="container8">  
+                <div class="content8">  
+                    
+                    </div>
+
+                    </div>
+
+                    <div class="container9">  
+                <div class="content9">  
+                    
+                    </div>
+
+                    </div>
+
+                    <div class="container10">  
+                <div class="content10">  
+                    
+                    </div>
+
+                    </div>
+                
+                    <div class="container11">  
+                <div class="content11">  
+                    
+                    </div>
+
+                    </div>
+
+                    <div class="container12">  
+                <div class="content12">  
+                    
+                    </div>
+
+                    </div>
+
+
+                    <div class="container13">  
+                <div class="content13">  
+                    
+                    </div>
+
+                    </div>
+
+           </MatriceStyled> 
+        </>
+    )
+}
